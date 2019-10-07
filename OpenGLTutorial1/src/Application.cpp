53783e5e1f018009c70a92fdc793f0eccdd2a66b
Alari Tork <alari.tork@amd.com>
2019-10-07 15:44:13 -0400

    Rendering first texture

--- OpenGLTutorial1/src/Application.cpp
+++ OpenGLTutorial1/src/Application.cpp
@@ -56,10 +56,10 @@ int main(void)
 
 		// Triangle vertices
 		float positions[] = {
-			-0.5f, -0.5f, //0
-			 0.5f, -0.5f, //1
-			 0.5f,  0.5f, //2
-			-0.5f,  0.5f, //3
+			-0.5f, -0.5f, 0.0f, 0.0f, //0
+			 0.5f, -0.5f, 1.0f, 0.0f, //1
+			 0.5f,  0.5f, 1.0f, 1.0f, //2
+			-0.5f,  0.5f, 0.0f, 1.0f  //3
 		};
 
 		// Index buffer
@@ -68,10 +68,14 @@ int main(void)
 			2, 3, 0  // Triangle 2
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		// CREATE VERTEX ARRAY OBJECT
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
@@ -82,7 +86,6 @@ int main(void)
 		// Create Shader
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
 		// Create texture
 		Texture texture("res/textures/n64.png");
@@ -92,31 +95,15 @@ int main(void)
 		// Create renderer
 		Renderer renderer;
 
-		// Red colour channel
-		float r = 0.0f;
-		float increment = 0.05f;
-
 		// MAIN LOOP
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			// Clear renderer
 			renderer.Clear();
-
-			// Set Colour
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			
+						
 			// Call draw
 			renderer.Draw(va, ib, shader);
-			
-			// Increment r to animate the red colour
-			if (r > 1.0f) {
-				increment = -0.05f;
-			}
-			else if (r < 0.0f) {
-				increment = 0.05f;
-			}
-			r += increment;
 
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
